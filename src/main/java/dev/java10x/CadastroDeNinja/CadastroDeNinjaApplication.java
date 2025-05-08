efab2b8060e83c1d6b78a47ce85062904e21cd75
Webber Chagas <webberchagas@gmail.com>
2025-05-08 17:30:20 -0300

    First commit

---
+++ src/main/java/dev/java10x/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeNinja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
