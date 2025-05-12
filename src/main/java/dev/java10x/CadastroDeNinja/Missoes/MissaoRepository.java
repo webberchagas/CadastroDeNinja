2c765e4de2709bd8e12a0cdca8701fcec6049e8b
Webber Chagas [HD] <webberschagas@gmail.com>
2025-05-12 16:46:56 -0300

    feat: Criado a camada Repository da classe Missao.

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinja.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
