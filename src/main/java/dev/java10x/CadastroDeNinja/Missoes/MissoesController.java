261081549b5cbe4285df69792ca61f9f0573b586
Webber Chagas <webberschagas@gmail.com>
2025-05-13 16:53:05 -0300

    feat: Criado as rotas do controller de Missoões com mock

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinja.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão Criada com sucesso";
+    }
+
+    @PutMapping("/altera")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada";
+    }
+}
