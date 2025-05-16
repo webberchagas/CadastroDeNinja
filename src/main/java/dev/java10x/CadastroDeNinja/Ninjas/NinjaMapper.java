6440f1b5c844c990c4db9c426695f634d4e8b61c
webberchagas <webberschagas@gmail.com>
2025-05-16 17:23:42 -0300

    chore: Criada a lógica do MAPPER e as propriedades do DTO

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaMapper.java
@@ -1,4 +1,34 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDto = new NinjaDTO();
+
+        ninjaDto.setId(ninjaModel.getId());
+        ninjaDto.setNome(ninjaModel.getNome());
+        ninjaDto.setEmail(ninjaModel.getEmail());
+        ninjaDto.setIdade(ninjaModel.getIdade());
+        ninjaDto.setMissoes(ninjaModel.getMissoes());
+        ninjaDto.setRank(ninjaModel.getRank());
+
+        return ninjaDto;
+    }
+
 }
